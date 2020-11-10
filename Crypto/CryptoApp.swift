7eb9c77ba6054bc8a277ad4280da19451ff469ae
Tandoh <anthony.tandoh@tanacom.io>
2020-11-10 11:11:10 +0000

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Tanacom on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
