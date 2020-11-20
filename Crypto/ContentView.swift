79410a1406dba86b6b8ab5203cb5f1d0dcdf9044
Tandoh <anthony.tandoh@tanacom.io>
2020-11-20 11:24:50 +0000

    commit Crypto ListView

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -7,18 +7,71 @@
 
 import SwiftUI
 
+
+struct Coin:Hashable {
+    let id,name,price,icon:String
+}
+
+
 struct ContentView: View {
+    
+    
+    var rates : [Coin] = [
+    Coin(id: "BTC", name: "BitCoin", price: "9793.31", icon: "bitcoin"),
+        
+    Coin(id: "LTC", name: "Litecoin", price: "93.08", icon: "litecoin"),
+        
+    Coin(id: "XIP", name: "Ripple", price: "0.98", icon: "ripple"),
+        
+    Coin(id: "TRX", name: "Tron", price: "23.10", icon: "tron"),
+        
+    Coin(id: "ETH", name: "Ethereum", price: "200.98", icon: "ethereum")
+        
+    ]
     
     var body: some View {
         
         NavigationView{
             
-            NavigationLink(destination: Text("Hello")){
+            
+            VStack{
                 
-                Text("Go to Hello")
+            
+            Text("Your crypto balance")
+                .padding()
+            
+                Text("$3,133.7")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
                     .padding()
+            
+               
+                List{
+                    
+                    Section(header: Text("Current Prices")){
+                        
+                        ForEach(rates,id: \.self){
+                            coin in
+                            Text(coin.name)
+                        
+                        }
+                    }
+                }
+                
             }
+                
+//            NavigationLink(destination: Text("Hello")){
+//
+//                Text("Go to Hello")
+//                    .padding()
+//            }
+            
            .navigationTitle(Text("DashBoard"))
+           .navigationBarHidden(true)
+            
+            
+            
+            
         }
     
     
