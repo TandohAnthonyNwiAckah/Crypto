3fb664b0ee99c45f7a7840b58c03804c92f4a3cd
Tandoh <anthony.tandoh@tanacom.io>
2020-11-22 07:40:52 +0000

    commit Constant

---
+++ Crypto/Utils/Cons.swift
@@ -0,0 +1,18 @@
+//
+//  Cons.swift
+//  Crypto
+//
+//  Created by Tanacom on 11/20/20.
+//
+
+import SwiftUI
+
+
+struct Cons {
+
+static let API =  "https://demo.tanacom.io/api.php"
+
+}
+    
+
+
